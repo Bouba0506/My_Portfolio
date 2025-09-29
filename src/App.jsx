0ea5188130aba6f4b356fa44739825b2f91493b6
Bah Boubacar <bahboubacar7272@gmail.com>
2025-09-29 12:04:47 +0200

    src

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes,Route } from "react-router"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import { Navbar } from "./components/Navbar"
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { Navbar } from "./Components/navbar";
 
 function App() {
-
   return (
     <>
-    <div className="bg-[#080001] h-full p-4 w-full ">
-    <Routes>
-      <Route index element={<Home/>}/>
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>
-    </div>
+      <div className="bg-[#080001] h-full p-4 w-full ">
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
